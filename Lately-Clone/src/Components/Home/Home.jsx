f307a379d25d8a443ab343408ca87345efb8445f
Hisam-94 <hisamuddin94@gmail.com>
2022-07-21 23:30:27 +0530

    Added AdvSec-1 and 2

--- Lately-Clone/src/Components/Home/Home.jsx
+++ Lately-Clone/src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import AdvSec_1 from "./AdvertisementSec - 1/AdvSec_1";
+import AdvSec_1 from "./AdvSec - 1/AdvSec_1";
+import AdvSec_2 from "./AdvSec - 2/AdvSec_2";
 import LogoBar from "./Logo Bar/LogoBar";
 import ResultSection from "./ResultSection/ResultSection";
 import TopSection from "./TopSection/TopSection";
@@ -15,6 +16,7 @@ const Home = () => {
       <VideoSection/>
       <ResultSection/>
       <AdvSec_1/>
+      <AdvSec_2/>
     </div>
   );
 };
